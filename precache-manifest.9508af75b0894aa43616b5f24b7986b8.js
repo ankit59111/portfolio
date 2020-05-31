104cf24f73f1bbb17b5f6f62ce2b19b78825bce7
online <ankitsingh5991111@gmail.com>
2020-06-01 00:59:05 +0530

    Updates

---
+++ precache-manifest.9508af75b0894aa43616b5f24b7986b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b47beb25287b85f51ad35656fa734f6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9e27a024ba9ecc0d35e",
+    "url": "/portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5f683cb429092d4a88c4",
+    "url": "/portfolio/static/css/main.b644a231.chunk.css"
+  },
+  {
+    "revision": "b9e27a024ba9ecc0d35e",
+    "url": "/portfolio/static/js/2.d47285ae.chunk.js"
+  },
+  {
+    "revision": "6766909b9304b6917a93c506d58bc65e",
+    "url": "/portfolio/static/js/2.d47285ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f683cb429092d4a88c4",
+    "url": "/portfolio/static/js/main.907681f9.chunk.js"
+  },
+  {
+    "revision": "46d8e4e4218b7a54a5a7",
+    "url": "/portfolio/static/js/runtime-main.531176fc.js"
+  }
+]);
